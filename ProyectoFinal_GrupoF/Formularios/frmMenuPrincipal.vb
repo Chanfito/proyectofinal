e07f37acada9f5bc035475d775bdcd2038d2f186
GJosue <gjosue0603@gmail.com>
2022-11-06 16:14:58 -0600

    Hice un cambio en las opciones del menu strip

--- ProyectoFinal_GrupoF/Formularios/frmMenuPrincipal.vb
+++ ProyectoFinal_GrupoF/Formularios/frmMenuPrincipal.vb
@@ -1,3 +1,5 @@
 ﻿Public Class frmMenuPrincipal
-
+    Private Sub menuOpciones_ItemClicked(sender As Object, e As ToolStripItemClickedEventArgs) Handles menuOpciones.ItemClicked
+        'Tomela playooo'
+    End Sub
 End Class
